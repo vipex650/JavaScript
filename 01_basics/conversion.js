d69defb3a407c77e6a54f7cca1271b8c8ff4c17c
ANUJ GUPTA <anujmodanwal708042@gmail.com>
2025-02-24 23:56:23 +0530

    conversion and expression

---
+++ 01_basics/conversion.js
@@ -0,0 +1,40 @@
+let num = "42"
+console.log(typeof num) // String
+
+let convertedNum = Number(num)
+console.log(convertedNum) // 42
+console.log(typeof convertedNum) // Number
+
+
+
+
+let str = 123
+let convertedString = String(str)
+console.log(typeof convertedString) // String
+
+
+let a = 2
+let b = 3
+let c = a**b // a power b 2*2*2
+console.log(c) // 8
+
+
+// Math
+console.log(Math.max(5,10))
+console.log(Math.min(5,10))
+
+console.log(Math.random()*10)
+
+
+
+
+
+// String
+let firstName = "Anuj"
+let LastName = "Gupta"
+
+let FullName = firstName + LastName // AnujGupta
+console.log(FullName)
+let fullname = firstName + " " + LastName;  // Anuj Gupta
+console.log(fullname)
+
